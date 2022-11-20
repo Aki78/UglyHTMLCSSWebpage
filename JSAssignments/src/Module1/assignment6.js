6e59a8854486c99d3751e2041cc7229e18de7271
Aki78 <kmorooka78@gmail.com>
2022-11-20 13:44:27 +0200

    done exercise 6

--- JSAssignments/src/Module1/assignment6.js
+++ JSAssignments/src/Module1/assignment6.js
@@ -1,15 +1,51 @@
-const Assignment1 = () => {
-    console.log("I'm printing to the console!")
+import { useEffect, useState } from "react";
+
+const Assignment6 = () => {
+    const [number, setNumber] = useState("");
+    const [c, setC] = useState("");
+
+
+
+    console.log(c)
+    useEffect(() => {
+        setC(window.confirm(
+            "are you sure"
+        ))
+    }, [])
+
+    console.log(c)
+    const handleConfirm = () => {
+        console.log("is true:", c)
+        if (c) {
+            return (
+                <div>
+                    <input type="text"
+                        value={number}
+                        onChange={(e) => setNumber(e.target.value)}
+                    />
+                    <div> {c.toString} {Math.sqrt(number)}</div>
+                </div>
+            )
+        }
+        else {
+            return (
+                <div> No computations</div>
+            )
+        }
+
+    }
     return (
         <>
-            <div>Assignment 1</div>
+            <div>Assignment6 </div>
+            {handleConfirm()}
         </>
-
     )
+
+
+
+
 }
 
-
-export default Assignment1;
-
+export default Assignment6;
 
 
