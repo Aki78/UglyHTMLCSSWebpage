dc4b343ef99aefba47e64f15a41e1afd174c02b3
Aki78 <kmorooka78@gmail.com>
2022-11-20 12:31:19 +0200

    done assignment 4

--- JSAssignments/src/Module1/assignment4.js
+++ JSAssignments/src/Module1/assignment4.js
@@ -1,15 +1,31 @@
-const Assignment1 = () => {
-    console.log("I'm printing to the console!")
+import { useState } from "react";
+
+const Assignment4 = () => {
+    const [studnetName, setStudentName] = useState("");
+    const [house, setHouse] = useState("");
+
+    const decideHouse = (e) => {
+        if (e.key === 'Enter') {
+            const houses = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"];
+            setHouse(houses[Math.floor(Math.random() * 4)]);
+        }
+    }
     return (
         <>
-            <div>Assignment 1</div>
+            <div>Assignment 4 </div>
+            <div>Press Enter after Name input</div>
+            <input type="text"
+                value={studnetName}
+                onChange={(e) => setStudentName(e.target.value)}
+                onKeyDown={(e) => decideHouse(e)}
+            />
+             <div> {studnetName}, you are {house}</div> 
+            
         </>
 
     )
 }
 
-
-export default Assignment1;
-
+export default Assignment4;
 
 
