74055f949a2d26999a8bc8007aded0bc58084059
Aki78 <kmorooka78@gmail.com>
2022-11-23 17:31:05 +0200

    done m2a8

--- JSAssignments/src/Module2/assignment8.js
+++ JSAssignments/src/Module2/assignment8.js
@@ -1,25 +1,14 @@
 import { useState } from "react";
 
 const M2Assignment8 = () => {
-    const [studnetName, setStudentName] = useState("");
-    const [house, setHouse] = useState("");
-
-    const decideHouse = (e) => {
-        if (e.key === 'Enter') {
-            const houses = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"];
-            setHouse(houses[Math.floor(Math.random() * 4)]);
-        }
+    const houses = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"];
+    const myConcat = (stem,e) => {
+        return stem + e
     }
     return (
         <>
             <div>Assignment 1 </div>
-            <div>Press Enter after Name input</div>
-            <input type="text"
-                value={studnetName}
-                onChange={(e) => setStudentName(e.target.value)}
-                onKeyDown={(e) => decideHouse(e)}
-            />
-             <div> {studnetName}, you are {house}</div> 
+            <div>{houses.reduce(myConcat)}</div>
             
         </>
 
