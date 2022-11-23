1887e2bb2742bc47e686ff51f299daed9f90bc1c
Aki78 <kmorooka78@gmail.com>
2022-11-23 16:39:29 +0200

    done m2a6

--- JSAssignments/src/Module2/assignment6.js
+++ JSAssignments/src/Module2/assignment6.js
@@ -1,31 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const M2Assignment6 = () => {
-    const [studnetName, setStudentName] = useState("");
-    const [house, setHouse] = useState("");
+    const [dices, setDices] = useState([]);
 
-    const decideHouse = (e) => {
-        if (e.key === 'Enter') {
-            const houses = ["Gryffindor", "Slytherin", "Hufflepuff", "Ravenclaw"];
-            setHouse(houses[Math.floor(Math.random() * 4)]);
+    const rollDice = () => Math.floor(Math.random() * 6 + 1);
+
+    var some_list = []
+useEffect (() => {
+    let rolls = (list) => {
+        const dice = rollDice()
+        // console.log(dice)
+        // console.log(dices)
+        if (dice != 6) {
+            return rolls([...list, dice])
+        }
+        else {
+            return ([...list, dice]);
+
         }
     }
+
+
+    setDices(rolls(some_list))
+    console.log("Final",dices)
+
+},[])
+
+
+
+
     return (
         <>
-            <div>Assignment 1 </div>
-            <div>Press Enter after Name input</div>
-            <input type="text"
-                value={studnetName}
-                onChange={(e) => setStudentName(e.target.value)}
-                onKeyDown={(e) => decideHouse(e)}
-            />
-             <div> {studnetName}, you are {house}</div> 
-            
+            <div>Module 2 Assignment 6 </div>
+            <ol> {dices.map((n, i) => <li>{n}</li>)} </ol>
+
         </>
 
     )
 }
 
 export default M2Assignment6;
-
 
