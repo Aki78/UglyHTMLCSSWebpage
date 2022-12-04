9a84d1ac2ffe979cfb92e057df8cd772612ebc49
Aki78 <kmorooka78@gmail.com>
2022-12-04 17:20:29 +0200

    done assignment 3_7

--- JSAssignments/src/Module3/assignment6.js
+++ JSAssignments/src/Module3/assignment6.js
@@ -1,9 +1,12 @@
-import { useEffect } from "react"
+import {useEffect} from "react"
 import {Helmet} from "react-helmet"
-const M3Assignment6 = () => {
+const M3Assignment7 = () => {
     useEffect ( () => {
-        const elem = document.querySelector("button")
-        elem.addEventListener('click', () => alert('Button clicked'));
+        const myImg = document.getElementById("target");
+        const hover = document.getElementById("trigger");
+
+        hover.addEventListener('mouseenter', () => myImg.src = process.env.PUBLIC_URL +  '/img/picB.jpg');
+        hover.addEventListener('mouseleave', () => {myImg.src = process.env.PUBLIC_URL +  '/img/picA.jpg'});
      }, [])
 
     return (
@@ -12,16 +15,17 @@ const M3Assignment6 = () => {
     <meta charset="UTF-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Task 6</title>
+    <title>Task 7</title>
             </Helmet>
 
-            <div>Module 3 Assignment 6 </div>
-    <h1>Task 6</h1>
-    <button>Click me!</button>
+            <div>Module 3 Assignment 7 </div>
+    <h1>Task 7</h1>
+    <img id="target" src= { process.env.PUBLIC_URL + "/img/picA.jpg"} alt="example"/>
+    <p id="trigger">Hover over me</p>
 
         </>
 
     )
 }
 
-export default M3Assignment6;
+export default M3Assignment7;
--- JSAssignments/src/Module3/assignment7.js
+++ JSAssignments/src/Module3/assignment7.js
@@ -1,52 +1,27 @@
-import { useEffect, useState } from "react";
-
-const M3Assignment7 = () => {
-    const [dices, setDices] = useState([]);
-    const [num, setNum] = useState(5);
-
-    const rollDice = () => Math.floor(Math.random() * 21 + 1);
-
-    let some_list = []
-
-    useEffect(() => {
-        let plays = 0
-        let rolls = (list, num_plays) => {
-            console.log("123", num_plays)
-            const dice = rollDice()
-            console.log("123", num_plays)
-            if (isNaN(num_plays) || num > 1000) return []
-
-            if (num_plays != num) {
-                num_plays += 1
-                return rolls([...list, dice], num_plays);
-            }
-            else {
-                console.log("HIT")
-                return ([...list, dice])
-            }
-        }
-
-        setDices(rolls(some_list, plays))
-        console.log("Final", dices)
-        plays = 0
-
-    }, [num])
-
+import { useEffect } from "react"
+import {Helmet} from "react-helmet"
+const M3Assignment6 = () => {
+    useEffect ( () => {
+        const elem = document.querySelector("button")
+        elem.addEventListener('click', () => alert('Button clicked'));
+     }, [])
 
     return (
         <>
-            <div>Module 3 Assignment 7 </div>
-            <input type="text"
-                value={num}
-                onChange={(e) => setNum(e.target.value)}
-                autoFocus
-            />
-            <ol> {dices.map((n) => <li>{n}</li>)} </ol>
+            <Helmet>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Task 6</title>
+            </Helmet>
+
+            <div>Module 3 Assignment 6 </div>
+    <h1>Task 6</h1>
+    <button>Click me!</button>
 
         </>
 
     )
 }
 
-export default M3Assignment7;
-
+export default M3Assignment6;
